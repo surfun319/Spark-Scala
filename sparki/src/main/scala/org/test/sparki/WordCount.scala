7e7de9e1d59989b37e1014ba2c0690b1e2c2ae90
surendra <surendra@surendra-ThinkPad-T410>
2016-05-26 14:24:54 +0530

    first Commit

---
+++ sparki/src/main/scala/org/test/sparki/WordCount.scala
@@ -0,0 +1,24 @@
+package org.test.sparki
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object WordCount {
+  
+  def main(args: Array[String])={
+    
+    val conf = new SparkConf()
+      .setAppName("WordCount")
+      .setMaster("local")
+    val sc = new SparkContext(conf)
+   
+    val test = sc.textFile("sample.txt")
+    
+    test.flatMap { line => line.split(" ")    }
+    .map { word => (word,1) }
+    .reduceByKey(_ + _)
+    .saveAsTextFile("sample.count.txt")
+    
+    
+  }
+}
